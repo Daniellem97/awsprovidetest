662278c8bf0da9f229a5193e21a5841576a97841
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2026-05-18 14:56:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-
-  assume_role_with_web_identity {
-    role_arn = var.aws_role_arn
-    web_identity_token_file = "/mnt/workspace/spacelift.oidc"
-  }
-}
-
-variable "aws_role_arn" {
-  description = "The ARN of the role to assume via OIDC"
-  type        = string
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-ibdfosubfub"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "random_pet" "orbit_mascot" {
+  length = 2
 }
